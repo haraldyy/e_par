44ee8e7b430efddff46ce5c23e3470f6f443be99
haraldyy <69537970+haraldyy@users.noreply.github.com>
2020-08-31 20:06:17 -0300

    Add files via upload

---
+++ e par.py
@@ -0,0 +1,10 @@
+def e_par():
+    n=int(input('Digite um número:'))
+    if n % 2 ==0:
+        print(True)
+    else:
+        print(False)
+def main():
+    e_par()
+if __name__=='__main__':
+    main()
